5a7ef527906abffafc0da0bd57a760d0fbb7993b
bcattaneo <c@ttaneo.uy>
2020-02-01 17:46:39 -0300

    Add main object handling

--- js/game.js
+++ js/game.js
@@ -1,25 +1,31 @@
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game-canvas', { preload: preload, create: create, render: render });
-
-function preload() {
-
-    // Switches
-    game.load.image('switch-on', 'assets/switch_on.png');
-    game.load.image('switch-off', 'assets/switch_off.png');
-
-}
+var game = new Phaser.Game(1200, 600, Phaser.CANVAS, 'game-canvas', { preload: preload, create: create, render: render, update: update });
 
 var debugText = '';
 
+function preload() {
+    preloadSwitches();
+
+    // TODO: move to machines.js
+}
+
 function create() {
     createSwitches();
+    createObject();
+}
+
+function update() {
+    // TODO: Test. Use actual matrix.
+    updateObject([
+        [{color: 0xfff, active: true}, {color: 0xFFFF00, active: true}, {color: 0xfff, active: true}],
+        [{color: 0xfff, active: true}, {color: 0x4b, active: true}, {color: 0xfff, active: false}],
+        [{color: 0xfff, active: true}, {color: 0xfff, active: true}, {color: 0xFFFF00, active: true}]
+    ]);
 }
 
 function render() {
-
     if (debugText === '') {
         game.debug.text("Interact with the switches.", 32, 32);
     } else {
         game.debug.text(debugText, 32, 32);
     }
-
 }
