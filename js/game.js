9034d3100bbd329ea5127408becd625935503da2
bcattaneo <c@ttaneo.uy>
2020-02-02 14:32:52 -0300

    Add missing assets

--- js/game.js
+++ js/game.js
@@ -10,46 +10,57 @@ var currentLevel = 0;
 var objects = [];
 
 var debugText = '';
-var bgMusic; // TODO
+var bgMusic;
 var anims = [];
 var platforms = [];
 var count;
 var countInt = 0;
+
+var bottom;
+
+var platformX = 0;
+var platformStep = 2;
 
 const platformY = 400;
 
 function preload() {
     preloadSwitches();
     preloadMachines();
-    // this.load.audio('bg_music', 'sounds/bg-music.ogg'); // TODO: Music
+    game.load.audio('bg_music', 'sounds/fabricaEspacial.wav');
     game.load.spritesheet('platform', 'assets/cinta.png', 201, 61, 2);
     game.load.image('bottom', 'assets/bottom.png');
+    game.load.image('marco', 'assets/marco.png');
+    game.load.image('back_simple', 'assets/back_simple.png');
 }
 
 function create() {
-    // TODO: Music
-    // bgMusic = this.sound.add('background_music');
-    // backgroundMusic.volume = 0.25;
-    // bgMusic.play();
 
-    game.add.image(0, 420, 'bottom');
+
+    // Background music
+    bgMusic = game.sound.add('bg_music');
+    bgMusic.loop = true;
+    bgMusic.volume = 0.25;
+    bgMusic.play();
+
+    game.add.image(0, 0, 'back_simple');
+    bottom = game.add.sprite(20, 420, 'bottom');
+    bottom.scale.setTo(0.95, 0.95);
 
     // Add platforms animation
-    // TODO: Move right and reset
-    platforms.push(game.add.sprite(-140, platformY, 'platform', 5));
-    anims.push(platforms[0].animations.add('move'));
-    anims[0].play(10, true);
+    for (var i = 1; i < 5; i++) {
+        platforms.push(game.add.sprite(140 * (-i), platformY, 'platform', 5));
+        anims.push(platforms[i - 1].animations.add('move'));
+        anims[i - 1].play(10, true);
+    }
     for (var i = 0; i < 30; i++) {
         platforms.push(game.add.sprite(140 * i, platformY, 'platform', 5));
-        anims.push(platforms[i + 1].animations.add('move'));
-        anims[i + 1].play(10, true);
+        anims.push(platforms[i + 4].animations.add('move'));
+        anims[i + 4].play(10, true);
     }
 
-    // anim.onStart.add(animationStarted, this);
-    // anim.onLoop.add(animationLooped, this);
-    // anim.onComplete.add(animationStopped, this);
+    createEdges();
 
-    count = game.add.text(670, 543, "0" + countInt, { fontSize: '15px', fill: '#000' });
+    count = game.add.text(670, 543, countInt, { fontSize: '15px', fill: '#000' });
     createObjects();
     objects.push({ x: objX, matrix: levels[currentLevel].entrada })
     createMachines();
@@ -57,13 +68,32 @@ function create() {
     for (let i = 0; i < machineObjects.length; i += 1) {
         machines[i].machineObject = machineObjects[i];
     }
+
+    game.add.image(0, 0, 'marco');
+    // game.add.sprite(0, 0, 'marco')
 }
 
 function update() {
+
     updateMachines();
     for (const object of objects) {
         checkCollision(object);
         updateObject(object);
+    }
+
+    platformX += platformStep;
+    // console.log(platformX);
+    if (platformX >= 278) {
+        // Reset platforms
+        platformX = 0;
+        for (var i = 1; i < 5; i++)
+            platforms[i - 1].x = 140 * (-i);
+        for (var i = 0; i < 30; i++)
+            platforms[i + 4].x = 140 * i;
+    } else {
+        // Update platforms movement
+        for (let platform of platforms)
+            platform.x += platformStep;
     }
 }
 
--- js/machines.js
+++ js/machines.js
@@ -1,4 +1,4 @@
-const maxY = 3.5;
+const maxY = 3.45;
 const firstMachineX = 200;
 const machinesY = -300;
 const secondMachineX = 600;
@@ -6,18 +6,25 @@ const machineGravity = 80;
 const machineWidth = 329;
 const machineHeight = 665;
 
+const edgeXOffset = 217.6;
+const edgeY = -43;
+
 const firstMachineName = "machine1";
 const secondMachineName = "machine2";
 
 var machines = [];
+var edges = [];
 
-// TODO: Check whether object got inside a machine
+/**
+ * 
+ * @param {*} object 
+ */
 function checkCollision(object) {
     for (const machine of machines)
         if (object.x === machine.position.x + 50) {
-            console.log(object.matrix);
+            // console.log(object.matrix);
             machine.machineObject.transform(object.matrix)
-            console.log(object.matrix);
+            // console.log(object.matrix);
         }
 }
 
@@ -25,14 +32,32 @@ function checkCollision(object) {
  * Preload for machines
  */
 function preloadMachines() {
-    game.load.image(firstMachineName, 'assets/maquina.png');
-    game.load.image(secondMachineName, 'assets/maquina.png');
+    game.load.image('palito', 'assets/palito.png');
+    game.load.image(firstMachineName, 'assets/maquina_1.png');
+    game.load.image(secondMachineName, 'assets/maquina_2.png');
+}
+
+/**
+ * 
+ */
+function createEdges() {
+    edges.push(game.add.sprite(firstMachineX + edgeXOffset, edgeY, 'palito'));
+    edges.push(game.add.sprite(secondMachineX + edgeXOffset, edgeY, 'palito'));
+
+    edges[0].scale.setTo(0.7, 0.7);
+    edges[1].scale.setTo(0.7, 0.7);
+
+    game.physics.enable(edges[0], Phaser.Physics.ARCADE);
+    game.physics.enable(edges[1], Phaser.Physics.ARCADE);
+    game.physics.arcade.gravity.y = machineGravity;
 }
 
 /**
  * Create logic for machines
  */
-function createMachines(machineObjects) {
+//function createMachines(machineObjects) {
+function createMachines() {
+
     machines.push(game.add.sprite(firstMachineX, machinesY, firstMachineName));
     machines.push(game.add.sprite(secondMachineX, machinesY, secondMachineName));
 
@@ -49,5 +74,7 @@ function updateMachines() {
     if (machines[0].deltaY >= maxY) {
         machines[0].body.moves = false;
         machines[1].body.moves = false;
+        edges[0].body.moves = false;
+        edges[1].body.moves = false;
     }
 }
