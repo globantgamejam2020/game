714951add19d29f4513590fe371a7f84a3699070
Sebastián Uriarte Güimil <sebastian.uriarteg@gmail.com>
2020-02-02 14:06:57 -0300

    js files

--- js/types.js
+++ js/types.js
@@ -2,108 +2,108 @@ const levels = [
     {
         entrada: [
             [
-                {color: parseInt("0xffffff", 16), active: true},
-                {color: parseInt("0xffffff", 16), active: true},
-                {color: parseInt("0xffffff", 16), active: true}
+                { color: parseInt("0xffffff", 16), active: true },
+                { color: parseInt("0xffffff", 16), active: true },
+                { color: parseInt("0xffffff", 16), active: true }
             ],
             [
-                {color: parseInt("0xffffff", 16), active: true},
-                {color: parseInt("0xffffff", 16), active: true},
-                {color: parseInt("0xffffff", 16), active: true}
+                { color: parseInt("0xffffff", 16), active: true },
+                { color: parseInt("0xffffff", 16), active: true },
+                { color: parseInt("0xffffff", 16), active: true }
             ],
             [
-                {color: parseInt("0xffffff", 16), active: true},
-                {color: parseInt("0xffffff", 16), active: true},
-                {color: parseInt("0xffffff", 16), active: true}
+                { color: parseInt("0xffffff", 16), active: true },
+                { color: parseInt("0xffffff", 16), active: true },
+                { color: parseInt("0xffffff", 16), active: true }
             ]
         ],
         salida: [
             [
-                {color: parseInt("0xffffff", 16), active: false},
-                {color: parseInt("0xffffff", 16), active: true},
-                {color: parseInt("0xffffff", 16), active: false}
+                { color: parseInt("0xffffff", 16), active: false },
+                { color: parseInt("0xffffff", 16), active: true },
+                { color: parseInt("0xffffff", 16), active: false }
             ],
             [
-                {color: parseInt("0xffffff", 16), active: false},
-                {color: parseInt("0xffffff", 16), active: true},
-                {color: parseInt("0xffffff", 16), active: false}
+                { color: parseInt("0xffffff", 16), active: false },
+                { color: parseInt("0xffffff", 16), active: true },
+                { color: parseInt("0xffffff", 16), active: false }
             ],
             [
-                {color: parseInt("0xffffff", 16), active: true},
-                {color: parseInt("0xffffff", 16), active: true},
-                {color: parseInt("0xffffff", 16), active: true}
+                { color: parseInt("0xffffff", 16), active: true },
+                { color: parseInt("0xffffff", 16), active: true },
+                { color: parseInt("0xffffff", 16), active: true }
             ]
         ]
     },
     {
         entrada: [
             [
-                {color: parseInt("0xffffff", 16), active: true},
-                {color: parseInt("0xffffff", 16), active: false},
-                {color: parseInt("0xffffff", 16), active: false}
+                { color: parseInt("0xffffff", 16), active: true },
+                { color: parseInt("0xffffff", 16), active: false },
+                { color: parseInt("0xffffff", 16), active: false }
             ],
             [
-                {color: parseInt("0xffffff", 16), active: true},
-                {color: parseInt("0xffffff", 16), active: false},
-                {color: parseInt("0xffffff", 16), active: false}
+                { color: parseInt("0xffffff", 16), active: true },
+                { color: parseInt("0xffffff", 16), active: false },
+                { color: parseInt("0xffffff", 16), active: false }
             ],
             [
-                {color: parseInt("0xffffff", 16), active: false},
-                {color: parseInt("0xffffff", 16), active: true},
-                {color: parseInt("0xffffff", 16), active: true}
+                { color: parseInt("0xffffff", 16), active: false },
+                { color: parseInt("0xffffff", 16), active: true },
+                { color: parseInt("0xffffff", 16), active: true }
             ]
         ],
         salida: [
             [
-                {color: parseInt("0xffffff", 16), active: true},
-                {color: parseInt("0xffffff", 16), active: true},
-                {color: parseInt("0xffffff", 16), active: false}
+                { color: parseInt("0xffffff", 16), active: true },
+                { color: parseInt("0xffffff", 16), active: true },
+                { color: parseInt("0xffffff", 16), active: false }
             ],
             [
-                {color: parseInt("0xffffff", 16), active: true},
-                {color: parseInt("0xffffff", 16), active: false},
-                {color: parseInt("0xffffff", 16), active: true}
+                { color: parseInt("0xffffff", 16), active: true },
+                { color: parseInt("0xffffff", 16), active: false },
+                { color: parseInt("0xffffff", 16), active: true }
             ],
             [
-                {color: parseInt("0xffffff", 16), active: false},
-                {color: parseInt("0xffffff", 16), active: true},
-                {color: parseInt("0xffffff", 16), active: true}
+                { color: parseInt("0xffffff", 16), active: false },
+                { color: parseInt("0xffffff", 16), active: true },
+                { color: parseInt("0xffffff", 16), active: true }
             ]
         ]
     },
     {
         entrada: [
             [
-                {color: parseInt("0xffffff", 16), active: false},
-                {color: parseInt("0xffffff", 16), active: false},
-                {color: parseInt("0xffffff", 16), active: true}
+                { color: parseInt("0xffffff", 16), active: false },
+                { color: parseInt("0xffffff", 16), active: false },
+                { color: parseInt("0xffffff", 16), active: true }
             ],
             [
-                {color: parseInt("0xffffff", 16), active: false},
-                {color: parseInt("0xffffff", 16), active: false},
-                {color: parseInt("0xffffff", 16), active: true}
+                { color: parseInt("0xffffff", 16), active: false },
+                { color: parseInt("0xffffff", 16), active: false },
+                { color: parseInt("0xffffff", 16), active: true }
             ],
             [
-                {color: parseInt("0xffffff", 16), active: true},
-                {color: parseInt("0xffffff", 16), active: true},
-                {color: parseInt("0xffffff", 16), active: true}
+                { color: parseInt("0xffffff", 16), active: true },
+                { color: parseInt("0xffffff", 16), active: true },
+                { color: parseInt("0xffffff", 16), active: true }
             ]
         ],
         salida: [
             [
-                {color: parseInt("0xffffff", 16), active: true},
-                {color: parseInt("0xffffff", 16), active: false},
-                {color: parseInt("0xffffff", 16), active: true}
+                { color: parseInt("0xffffff", 16), active: true },
+                { color: parseInt("0xffffff", 16), active: false },
+                { color: parseInt("0xffffff", 16), active: true }
             ],
             [
-                {color: parseInt("0xffffff", 16), active: true},
-                {color: parseInt("0xffffff", 16), active: false},
-                {color: parseInt("0xffffff", 16), active: true}
+                { color: parseInt("0xffffff", 16), active: true },
+                { color: parseInt("0xffffff", 16), active: false },
+                { color: parseInt("0xffffff", 16), active: true }
             ],
             [
-                {color: parseInt("0xffffff", 16), active: true},
-                {color: parseInt("0xffffff", 16), active: true},
-                {color: parseInt("0xffffff", 16), active: true}
+                { color: parseInt("0xffffff", 16), active: true },
+                { color: parseInt("0xffffff", 16), active: true },
+                { color: parseInt("0xffffff", 16), active: true }
             ]
         ]
     }
