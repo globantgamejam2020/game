caac11c4b724bf17f2b20c4cf2abee18032f083c
Sebastián Uriarte Güimil <sebastian.uriarteg@gmail.com>
2020-02-02 17:11:14 -0300

    added level increase and win condition

--- js/game.js
+++ js/game.js
@@ -6,7 +6,7 @@ var game = new Phaser.Game(
     true
 );
 
-var currentLevel = 0;
+var currentLevel = -1;
 var objects = [];
 
 var debugText = '';
@@ -53,6 +53,8 @@ function preload() {
     game.load.image('back_simple', 'assets/back_simple.png');
     game.load.image('rrr_splash', 'assets/RRR.png');
     game.load.image('enter_game', 'assets/enter_game.png');
+    game.load.image('enter_game', 'assets/RRR_VICTORIA.png');
+    game.load.image('enter_game', 'assets/RRR_DERROTA.png');
 
     game.load.spritesheet('platform', 'assets/cinta.png', 201, 61, 2);
     game.load.spritesheet('speaker-anim', 'assets/speaker.png', 154, 158, 2);
@@ -79,7 +81,6 @@ function create() {
     createEdges();
 
     count = game.add.text(670, 543, countInt, { fontSize: '15px', fill: '#000' });
-    createObjects();
     createMachines();
     const machineObjects = createSwitches();
     for (let i = 0; i < machineObjects.length; i += 1) {
@@ -98,12 +99,11 @@ function create() {
     speaker = game.add.sprite(790, 410, 'speaker-anim', 5);
     speaker.animations.add('play-sound');
     speaker.play(10, true); // TODO: fix
-
-    updateGoalObject();
+    navigateToNextLevel();
 }
 
 function updateGoalObject() {
-    if (goalObject && goalObject.level === currentLevel) return;
+    if (!gameStarted || (goalObject && goalObject.level === currentLevel)) return;
     if (goalObject) goalObject.graphics.destroy();
     const matrix = levels[currentLevel].salida
     goalObject = { level: currentLevel, matrix };
@@ -123,15 +123,14 @@ function updateGoalObject() {
 
 function update() {
     if (!gameStarted) {
-        // startGame(undefined);
+        startGame(undefined);
     } else {
         updateMachines();
         for (const object of objects) {
             checkCollision(object);
             updateObject(object);
-            checkSolution(object);
         }
-    
+
         platformX += platformStep;
         // console.log(platformX);
         if (platformX >= 278) {
@@ -147,7 +146,6 @@ function update() {
                 platform.x += platformStep;
         }
     }
-    updateGoalObject();
 }
 
 function render() {
@@ -158,23 +156,16 @@ function render() {
     }
 }
 
-function checkSolution(object) {
-    if (!goalObject || !goalObject.matrix) return;
-    let solved = true;
-    let i = 0;
-    let j = 0;
-
-    while (i < 3 && solved) {
-        while (j < 3 && solved) {
-            solved = object.matrix[i][j].active === goalObject.matrix[i][j].active && object.matrix[i][j].color === goalObject.matrix[i][j].color;
-            j++;
-        }
-        i++;
+function navigateToNextLevel() {
+    currentLevel += 1;
+    if (currentLevel === end) {
+        showSuccess();
     }
+    updateGoalObject();
+    createObjects();
+    countInt = 35;
+}
 
-    // TODO: esto tiene que cambiar algo en el juego... Sumar un contador o whatever.
-    if (solved)
-        console.info('Llegaste a la solucion');
-    else
-        console.info('No sabes nada');
+function showSuccess() {
+    game.add.sprite(0, 0, 'RRR_VICTORIA', 5);
 }
